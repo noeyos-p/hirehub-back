6542243e4bef64a2ead744c26b6ad549358cd64e
MINSsss96 <minsue0422@naver.com>
2025-10-28 10:15:17 +0900

    공고 스크랩 (북마크 설정, 해제) -완-, 기업 즐겨찾기 (설정, 해제) 90%2

---
+++ src/main/java/com/we/hirehub/dto/JobPostsDto.java
@@ -0,0 +1,25 @@
+package com.we.hirehub.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobPostsDto {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDate startAt;
+    private LocalDate endAt;
+    private String location;
+    private String careerLevel;
+    private String education;
+    private String type;
+    private String salary;
+    private String companyName;
+    private Long companyId;  // ⭐ 이 필드 추가
+}
