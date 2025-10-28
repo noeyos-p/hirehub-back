e531bbcbe35daf863545fd9f2858269153899401
kumanamu <serachstock1@gmail.com>
2025-10-28 19:58:42 +0900

    어드민페이지의 이력서관리용 레포지토리 추가 및 컨트롤러 수정

--- src/main/java/com/we/hirehub/controller/AdminController.java
+++ src/main/java/com/we/hirehub/controller/AdminController.java
@@ -1,10 +1,23 @@
 package com.we.hirehub.controller;
 
-import org.springframework.stereotype.Controller;
+import com.we.hirehub.dto.ResumeDto;
+import com.we.hirehub.service.ResumeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/admin/resume-management")
+@RequiredArgsConstructor
 public class AdminController {
+
+
+    private final ResumeService resumeService;
 
     @GetMapping("/admin")
     public String adminHome(){
@@ -15,4 +28,11 @@ public class AdminController {
     public String adminSupport(){
         return "support/agent"; // templates/support/agent.html (대기열/수락)
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> getResumeById(@PathVariable Long id) {
+        ResumeDto dto = resumeService.toDtoForAdmin(id);
+        return ResponseEntity.ok(Map.of("success", true, "data", dto));
+    }
 }
+
