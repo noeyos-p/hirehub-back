b898506722ce19e6eaaf4a51269ad71d9f478055
MINSsss96 <minsue0422@naver.com>
2025-10-29 16:39:49 +0900

    userid 저장

--- src/main/java/com/we/hirehub/dto/LiveChatDto.java
+++ src/main/java/com/we/hirehub/dto/LiveChatDto.java
@@ -10,5 +10,7 @@ public class LiveChatDto {
     private String content;
     private LocalDateTime createAt;
     private String sessionId; // LiveChat.session (@ManyToOne) -> Session.id
+    private Long userId;      // Users ID 추가
     private String nickname;  // 추가
+
 }
