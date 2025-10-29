8040ccebc6c4a0850916b0a9acb78d9a367731ed
kumanamu <serachstock1@gmail.com>
2025-10-29 17:12:20 +0900

    챗봇구현중

--- src/main/java/com/we/hirehub/handoff/HandoffController.java
+++ src/main/java/com/we/hirehub/handoff/HandoffController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * 핸드오프 REST 엔드포인트
+ * - /api/handoff?roomId=xxx  (대기중 목록)
+ * - POST /api/handoff        (요청 생성)
+ */
 @RestController
 @RequestMapping("/api/handoff")
 @RequiredArgsConstructor
@@ -15,12 +20,12 @@ public class HandoffController {
     private final HandoffService service;
 
     @GetMapping
-    public ResponseEntity<List<HandoffDto>> getPending(@RequestParam String roomId){
+    public ResponseEntity<List<HandoffDto>> getPending(@RequestParam String roomId) {
         return ResponseEntity.ok(service.findPendingByRoomId(roomId));
     }
 
     @PostMapping
-    public ResponseEntity<HandoffDto> create(@RequestBody HandoffService.HandoffRequest request){
+    public ResponseEntity<HandoffDto> create(@RequestBody HandoffService.HandoffRequest request) {
         return ResponseEntity.ok(service.create(request));
     }
 }
